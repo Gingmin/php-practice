dd499a0b9f8eb3433e348b7d7596110409089210
DESKTOP-65A3TKA\ging <ging@purplemint.com>
2021-06-23 11:46:45 +0900

    first commit

---
+++ function.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>FUNCTION</title>
+</head>
+<body>
+    <h1>function</h1> 
+    <?php 
+    $str = "Lorem ipsum 
+    
+    dolor sit amet";
+    echo $str;
+    ?>
+    <h2>strlen()</h2>
+    <?php
+    echo strlen($str);
+    ?>
+    <h2>nl2br</h2>
+    <?php 
+    echo nl2br($str);
+    ?>
+</body>
+</html>
